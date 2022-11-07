1f8f8b5b601228e673f2df46a42ffbb7160cb4a3
sebastian_kup <sebastian.kuppa@web.de>
2022-11-07 13:48:06 +0800

    added BasicBlock class

---
+++ resnet18.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torch
+from torch import Tensor
+
+from typing import Type
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, expansion: int = 1,
+                 downsample: nn.Module = None) -> None:
+        super(BasicBlock, self).__init__()
+        self.expansion = expansion #multiplicative factor for subsequentconv2d layers, 1 for resnet18 and 34
+        self.downsample = downsample
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels*self.expansion)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
