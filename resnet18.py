6f145888db9e42314623dd7d4b8cc5660826ea2c
sebastian_kup <sebastian.kuppa@web.de>
2022-11-08 16:05:23 +0800

    added ResNet class with init function

--- resnet18.py
+++ resnet18.py
@@ -33,3 +33,34 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
 
         return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, img_channels: int, num_layers: int, block: Type[BasicBlock], num_classes: int = 1000) -> None:
+        super(ResNet, self).__init__()
+        if num_layers == 18:
+            # the following variable defines how many 'BasicBlock's should be used and how they are staked together
+            layers = [2, 2, 2, 2]
+            self.expansion = 1
+
+        self.in_channels = 64
+        # ResNets (18 to 152) contain: Conv2d->BN->ReLU
+        self.conv1 = nn.Conv2d(in_channels=img_channels,
+                               out_channels=self.in_channels,
+                               kernel_size=7,
+                               stride=2,
+                               padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512*self.expansion, num_classes)
+
+
