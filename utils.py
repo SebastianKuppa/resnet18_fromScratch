cb0c4c1bac5e667c5bb80362cc0ef41f1c1f64e2
sebastian_kup <sebastian.kuppa@web.de>
2022-11-12 17:05:39 +0800

    added save_plots() function to utils.py

--- utils.py
+++ utils.py
@@ -37,3 +37,42 @@ def get_data(batch_size=64):
     )
 
     return train_loader, validation_loader
+
+def save_plots(train_acc, valid_acc, train_loss, valid_loss, name=None):
+    """
+    Saves training and validation accuracy/loss on disk
+    :param train_acc: training accuracy
+    :param valid_acc: validation accuracy
+    :param train_loss: training loss
+    :param valid_loss: validation loss
+    :param name: file name
+    """
+    # accuracy plots
+    plt.figure(figsize=(10, 7))
+    plt.plot(
+        train_acc, color='tab:blue', linestyle='-',
+        label='train_accuracy'
+    )
+    plt.plot(
+        valid_acc, color='tab:red', linestyle='-',
+        label='validation_accuracy'
+    )
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig(os.path.join('outputs', name + '_accuracy.png'))
+
+    # loss plots
+    plt.figure(figsize=(10, 7))
+    plt.plot(
+        train_loss, color='tab:blue', linestyle='-',
+        label='train_loss'
+    )
+    plt.plot(
+        valid_loss, color='tab:red', linestyle='-',
+        label='validation_loss'
+    )
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join('outputs', name + '_loss.png'))
