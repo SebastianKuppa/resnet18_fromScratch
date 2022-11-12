140d025c5b582b7c48cb374f2df6c3863dd7a1f5
sebastian_kup <sebastian.kuppa@web.de>
2022-11-12 16:30:42 +0800

    added get_data() function to utils.py

--- utils.py
+++ utils.py
@@ -9,11 +9,31 @@ plt.style.use('ggplot')
 
 
 def get_data(batch_size=64):
-    #CIFAR10 dataset
+    # CIFAR10 dataset for training
     dataset_train = datasets.CIFAR10(
         root='data',
         train=True,
         download=False,
         transform=ToTensor(),
         )
+    # CIFAR10 dataset for validation
+    dataset_validate = datasets.CIFAR10(
+        root='data',
+        train=False,
+        download=False,
+        transform=ToTensor(),
+    )
+    # init training dataloader
+    train_loader = DataLoader(
+        dataset_train,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    # init validation dataloader
+    validation_loader = DataLoader(
+        dataset_validate,
+        batch_size=batch_size,
+        shuffle=False
+    )
 
+    return train_loader, validation_loader
