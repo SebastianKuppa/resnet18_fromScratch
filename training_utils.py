17b15a540b11eb3388f527be70c868bb6b943452
sebastian_kup <sebastian.kuppa@web.de>
2022-11-13 17:34:33 +0800

    addded validation function to training_utils.py

--- training_utils.py
+++ training_utils.py
@@ -3,10 +3,10 @@ import torch
 from tqdm import tqdm
 
 
-# training function
+# network training
 def train(model, trainloader, optimizer, criterion, device):
     model.train()
-    print('Training..')
+    print('Starting network training..')
     train_running_loss = 0.0
     train_running_correct = 0
     counter = 0
@@ -32,4 +32,30 @@ def train(model, trainloader, optimizer, criterion, device):
     # loss and acc for whole epoch
     epoch_loss = train_running_loss / counter
     epoch_acc = 100. * (train_running_correct/len(trainloader.dataset))
+    return epoch_loss, epoch_acc
+
+
+# network validation
+def validate(model, testloader, criterion, device):
+    model.eval()
+    print('Starting network validation..')
+    valid_running_loss = 0.0
+    valid_running_correct = 0
+    counter = 0
+    for i, data in tqdm(enumerate(testloader), total=len(testloader)):
+        counter += 1
+        image, labels = data
+        image = image.to(device)
+        labels = labels.to(device)
+        # forward pass
+        outputs = model(image)
+        loss = criterion(outputs, labels)
+        valid_running_loss += loss.item()
+        # calc accuracy
+        _, preds = torch.max(outputs.data, 1)
+        valid_running_correct += (preds == labels).sum().item()
+
+    # loss and acc for whole epoch
+    epoch_loss = valid_running_loss / counter
+    epoch_acc = 100. * (valid_running_correct/len(testloader.dataset))
     return epoch_loss, epoch_acc
