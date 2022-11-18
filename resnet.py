118388e4015d8d575ec7ede2be982e6e13956fa6
sebastian_kup <sebastian.kuppa@web.de>
2022-11-18 14:51:23 +0800

    added ResNet block class

--- resnet.py
+++ resnet.py
@@ -16,10 +16,11 @@ args = vars(parser.parse_args())
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, num_layers:int, in_channels:int, out_channels:int, stride:int = 1, expansion:int = 1,
+    def __init__(self, num_layers: int, in_channels: int, out_channels: int, stride: int = 1, expansion: int = 1,
                  downsample: nn.Module = None) -> None:
         super(BasicBlock, self).__init__()
         self.num_layers = num_layers
+        #
         # multiplicative factor for the subsequent conv2d layers output channels
         # it is 1 for resnet 18/35 and 4 for the rest
         self.expansion = expansion
@@ -88,3 +89,35 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
 
         return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, img_channels: int, num_layers: int, block: Type[BasicBlock], num_classes: int = 1000) -> None:
+        super(ResNet, self).__init__()
+        if num_layers == 18:
+            layers = [2, 2, 2, 2]
+            self.expansion = 1
+        if num_layers == 34:
+            layers = [3, 4, 6, 3]
+            self.expansion = 1
+        if num_layers == 50:
+            layers = [3, 4, 6, 3]
+            self.expansion = 4
+        if num_layers == 101:
+            layers = [3, 4, 23, 3]
+            self.expansion = 4
+        if num_layers == 152:
+            layers = [3, 8, 36, 3]
+            self.expansion = 4
+
+        self.in_channels = 64
+
+        # every ResNet contains a Conv2D(kernel_size=7), BN, Relu at the beginning
+        self.conv1 = nn.Conv2d(
+            in_channels=img_channels,
+            out_channels=self.in_channels,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False
+        )
